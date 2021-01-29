9b7f98cbc942db2e913bb5b1e8c77648d86e3e82
Odrigor <rodcid.al@gmail.com>
2021-01-28 23:15:04 -0300

    Avanze 1

--- webpack.config.js
+++ webpack.config.js
@@ -52,7 +52,16 @@ module.exports = {
                     'css-loader',
                     'sass-loader'
                 ]
-      }
+      },
+      {
+        test: /\.(png|gif|jpg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: { name: 'assets/[hash].[ext]' },
+          }
+        ],
+      },
     ]
   },
   // Se añaden los plugins que necesitamos
